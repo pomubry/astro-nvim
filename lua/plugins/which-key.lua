59228df94d8c22cfbec23747aaaca308332f50b9
BTaduran <btaduran30@protonmail.com>
2026-06-08 15:32:01 +0800

    change which-key preset to helix

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- Choose a preset layout style
+    -- "classic" = bottom pane across full width (traditional look)
+    -- "modern"  = floating window centered at the bottom
+    -- "helix"   = floating window split on the right side
+    preset = "helix",
+  },
+}
